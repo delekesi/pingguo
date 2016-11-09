1f391d131afc491864fcb424131c5ed451217c7c
delekesi <673745069@qq.com>
2016-11-09 11:09:15 +0800

    pingguo2
    pingguo

--- js/index.js
+++ js/index.js
@@ -51,7 +51,7 @@ var todo=[
 ]
 var colors=["#1BAEF8","#67DC3A","#FF8800","#D07BE3","#F7CC00","#A3855F","#FF2762"];
 var app=angular.module("app",[])
-app.controller("cc",function($scope,localstg) {
+app.controller("cc",function($scope,localstg){
     // $scope.todo=todo;
     // localstg.saveData('todo',$scope.todo)
     $scope.todo=localstg.getData("todo")
@@ -69,42 +69,42 @@ app.controller("cc",function($scope,localstg) {
          $scope.index=i;
           $scope.chtt = $scope.todo[i].title;
           $scope.changeColor = $scope.todo[i].color;
-          $scope.flag=false;
+         
 
       }
-
-    $scope.down=0;
-
-
-    $scope.getdown=function () {
-       $scope.down=0;
-        var list=$scope.todo[$scope.index].list;
-        angular.forEach(list,function (v,l) {
-          if(v.done){
-               $scope.down+=1;
-          }
-        })
-        localstg.saveData("todo",$scope.todo)
-    }
-    $scope.getdown()
-    $scope.$watch("index",function () {
-        $scope.getdown();
-        $scope.flag=false;
-        $scope.chtt = $scope.todo[$scope.index].title;
-        $scope.changeColor = $scope.todo[$scope.index].color;
-        localstg.saveData("todo",$scope.todo)
-
-    })
-    $scope.bianhua=function (o,f) {
+        $scope.bianhua=function(o,f) {
           o.done=f;
         $scope.getdown()
         localstg.saveData("todo",$scope.todo)
     }
-    $scope.change=function (o,text) {
+
+       $scope.clearbox=function() {
+        var list=$scope.todo[$scope.index].list;
+        var shuzu=[];
+        angular.forEach(list,function(v,i) {
+           if(v.done==false){
+               shuzu.push(v)
+           }
+        })
+
+        $scope.todo[$scope.index].list=shuzu
+     
+        $scope.optflag=false;
+           $scope.getdown();
+        localstg.saveData("todo",$scope.todo)
+
+    }
+    $scope.sColor=function (c) {
+       $scope.changeColor = c;
+
+    }
+
+      $scope.change=function (o,text) {
         o.title=text.target.innerHTML
         localstg.saveData("todo",$scope.todo)
     }
-    $scope.addItem=function () {
+
+     $scope.addItem=function () {
         $scope.ids=$scope.todo[$scope.todo.length-1].id+1
          $scope.index=$scope.todo.length;
         $scope.todo.push({
@@ -115,10 +115,11 @@ app.controller("cc",function($scope,localstg) {
         })
 
 
-
+        $scope.getdown();
         localstg.saveData("todo",$scope.todo)
 
     }
+
 
     $scope.add=function () {
         $scope.todo[$scope.index].list.push({
@@ -128,35 +129,9 @@ app.controller("cc",function($scope,localstg) {
         $scope.getdown()
         localstg.saveData("todo",$scope.todo)
     }
-    $scope.clearbox=function () {
-        var list=$scope.todo[$scope.index].list;
-        var shuzu=[];
-        angular.forEach(list,function (v,i) {
-           if(v.done==false){
-               shuzu.push(v)
-           }
-        })
 
-        $scope.todo[$scope.index].list=shuzu
-        $scope.getdown();
-        $scope.optflag=false;
-        localstg.saveData("todo",$scope.todo)
 
-    }
-    $scope.sColor=function (c) {
-       $scope.changeColor = c;
-
-    }
-    
-
-    $scope.comcc = function () {
-        var o = $scope.todo[$scope.index];
-        o.title=$scope.chtt;
-        o.color=$scope.changeColor;
-        $scope.optflag=false;
-        localstg.saveData("todo",$scope.todo)
-    }
-    $scope.dellist=function () {
+      $scope.dellist=function () {
         if($scope.todo.length==1){
             alert("至少保留一个")
             return;
@@ -165,11 +140,51 @@ app.controller("cc",function($scope,localstg) {
         $scope.todo.splice($scope.index,1);
         $scope.index=$scope.todo.length-1;
         $scope.optflag=false
-
         localstg.delData('todo');
+        localstg.saveData('todo',$scope.todo)
 
 
     }
+
+       $scope.comcc = function () {
+        var o = $scope.todo[$scope.index];
+        o.title=$scope.chtt;
+        o.color=$scope.changeColor;
+        $scope.optflag=false;
+        localstg.saveData("todo",$scope.todo)
+    }
+    $scope.down=0;
+
+
+    $scope.getdown=function () {
+       $scope.down=0;
+        var list=$scope.todo[$scope.index].list;
+        angular.forEach(list,function(v) {
+          if(v.done){
+               $scope.down+=1;
+          }
+        })
+        
+    }
+    $scope.getdown()
+    $scope.$watch("index",function () {
+        $scope.getdown();
+        $scope.chtt = $scope.todo[$scope.index].title;
+        $scope.changeColor = $scope.todo[$scope.index].color;
+
+
+    })
+  
+  
+   
+
+    
+ 
+   
+    
+
+ 
+  
 
 })
 
@@ -177,14 +192,16 @@ app.controller("cc",function($scope,localstg) {
 app.factory("localstg",function(){
     return {
         getData:function (key) {
-        var d=localStorage.getItem(key);
-            return d==null?[]:JSON.parse(d);
+        var d=localStorage.getItem('todo');
+            if(d!=null){
+                return JSON.parse(d);
+                            }else return [];
         },
         saveData:function (key,data) {
-          localStorage.setItem(key,JSON.stringify(data))
+          localStorage.setItem('todo',JSON.stringify(data))
         },
         delData:function (key) {
-           localStorage.removeItem(key);
+           localStorage.removeItem('todo');
         }
     }
 })
